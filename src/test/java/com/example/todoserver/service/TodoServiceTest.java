2c00dfe2bc3403bfcabf4aa95169505ce6d444c9
hongmj37 <alswjd3813@naver.com>
2022-07-21 10:49:47 +0900

    테스트 진행중

---
+++ src/test/java/com/example/todoserver/service/TodoServiceTest.java
@@ -0,0 +1,65 @@
+package com.example.todoserver.service;
+
+import com.example.todoserver.model.TodoEntity;
+import com.example.todoserver.model.TodoRequest;
+import com.example.todoserver.repository.TodoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceTest {
+
+    @Mock
+    private TodoRepository todoRepository;
+
+    @InjectMocks
+    private TodoService todoService;
+
+    @Test
+    void add() {
+        when(this.todoRepository.save(any(TodoEntity.class)))
+                .then(AdditionalAnswers.returnsFirstArg());
+
+        TodoRequest expected = new TodoRequest();
+        expected.setTitle("Test title");
+
+        TodoEntity actual = this.todoService.add(expected);
+
+        assertEquals(expected.getTitle(), actual.getTitle());
+    }
+
+    @Test
+    void searchById() {
+        TodoEntity entity = new TodoEntity();
+        entity.setId(123L);
+        entity.setTitle("Title");
+        entity.setOrder(0L);
+        entity.setCompleted(false);
+        Optional<TodoEntity> optional = Optional.of(entity);
+
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(optional);
+
+        TodoEntity actual = this.todoService.searchById(123L);
+
+        TodoEntity expected = optional.get();
+
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getTitle(), actual.getTitle());
+    }
+
+
+}
