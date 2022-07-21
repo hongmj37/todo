47e28f7d2d6a3931b82ab2bd8a5ccaee7a064a8f
hongmj37 <alswjd3813@naver.com>
2022-07-21 10:49:38 +0900

    테스트 진행중

--- src/main/java/com/example/todoserver/TodoServerApplication.java
+++ src/main/java/com/example/todoserver/TodoServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodoServerApplication.class, args);
 	}
